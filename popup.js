872787e8efb2280285007be6898e6b5053d48b99
tinyjin <baram991103@gmail.com>
2019-03-19 21:50:13 +0900

    확장 사용 여부 - storage에 저장

--- popup.js
+++ popup.js
@@ -1,4 +1,24 @@
 const blogBtn = document.querySelector('.blog-btn');
+
 blogBtn.onclick = () => {
   chrome.tabs.create({ url: 'https://wlsdml1103.blog.me' });
+};
+
+const switchInput = document.querySelector('.switch-input');
+
+window.onload = () => {
+  chrome.storage.sync.get(['isOn'], function (result) {
+    const { isOn } = result;
+
+    switchInput.checked = isOn || false;
+    console('Value currently is ' + isOn);
+  });
+};
+
+switchInput.onclick = function (event) {
+  const changeStatus = event.target.checked;
+
+  chrome.storage.sync.set({isOn: changeStatus}, () => {
+    console('Value is set to ' + changeStatus);
+  });
 };
