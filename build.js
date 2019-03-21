51cae2e787e2f3157e5d1d67b2474900daeb66d2
tinyjin <baram991103@gmail.com>
2019-03-21 12:28:51 +0900

    팝업 코드, 컨텐츠 스크립트 코드 - 난독화 및 압축

--- build.js
+++ build.js
@@ -3,28 +3,32 @@ const path = require('path');
 const babel = require('@babel/core');
 
 const build = () => {
-  const { code } = runBabel();
-  makeFile(code);
+  const { code : extensionCode } = runBabel('extension.js');
+  const { code : popupCode } = runBabel('popup.js');
+  const { code : contentScriptCode } = runBabel('contentscript.js');
+
+  makeFile(extensionCode, popupCode, contentScriptCode);
 };
 
-const makeFile = (code) => {
+const makeFile = (extensionCode, popupCode, contentScriptCode) => {
   // 디렉터리 재생성
   // fs.rmdirSync(path.join(__dirname, 'dist'));
   fs.mkdirSync(path.join(__dirname, 'dist'));
 
   // extension.js 빌드
-  fs.writeFileSync(path.join(__dirname, 'dist', 'extension.js'), code, 'utf8');
+  fs.writeFileSync(path.join(__dirname, 'dist', 'extension.js'), extensionCode, 'utf8');
+  // popup.js 빌드
+  fs.writeFileSync(path.join(__dirname, 'dist', 'popup.js'), popupCode, 'utf8');
+  // contentscript.js 빌드
+  fs.writeFileSync(path.join(__dirname, 'dist', 'contentscript.js'), contentScriptCode, 'utf8');
 
   // 기타 파일 복사
-  fs.copyFileSync(path.join(__dirname, 'contentscript.js'), path.join(__dirname, 'dist', 'contentscript.js'));
   fs.copyFileSync(path.join(__dirname, 'manifest.json'), path.join(__dirname, 'dist', 'manifest.json'));
   fs.copyFileSync(path.join(__dirname, 'popup.html'), path.join(__dirname, 'dist', 'popup.html'));
-  fs.copyFileSync(path.join(__dirname, 'popup.js'), path.join(__dirname, 'dist', 'popup.js'));
-
 };
 
-const runBabel = () => {
-  return babel.transformFileSync('extension.js');
+const runBabel = (file) => {
+  return babel.transformFileSync(file);
 };
 
 
